1bab5df1b0ad0d556cd06bc979cadbb331fd96c7
sakethpabbathi <sakethpabbathi@gmail.com>
2026-03-07 10:33:02 +0530

    2nd

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import React from "react";
-import ReactDOM from "react-dom/client";
+// import App from "./App";
+// import { BrowserRouter } from "react-router-dom";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+// root.render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>
+// );
+
+import { HashRouter, Route, Routes } from "react-router-dom";
+
+import About from "./About";
+import HomePage from "./HomePage";
+
+function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </HashRouter>
+  );
+}
+
+export default App;
